e03ab6e4c2c88dec14b4123f8891e049218da31c
Jeremy Siegelstein <Jeremy@Jeremys-MacBook-Air.local>
2020-03-10 22:41:33 -0400

    Litty

--- tcp_client_x.cpp
+++ tcp_client_x.cpp
@@ -4,53 +4,69 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#define MAX 80
-#define PORT 8888
-void func(int sockfd)
+
+#define PORT 12345
+//CLIENTS X AND Y HAVE THE SAME CODE
+void foo(int sockfd)
 {
-    char buff[MAX];
+    char buff[1024];
     int n;
         bzero(buff, sizeof(buff));
-        printf("Client X: ");
+        std::cout << "Client X: " << std::endl;
         n = 0;
-        while ((buff[n++] = getchar()) != '\n');
+        //Populate buffer with your message
+        while ((buff[n++] = getchar()) != '\n' && n < buff);
+        //Get rid of the newline character
 		buff[n-1] = '\0';
+        //Write to the server
         write(sockfd, buff, sizeof(buff));
+        //Clear the buffer
         bzero(buff, sizeof(buff));
+        //Read from the server
         read(sockfd, buff, sizeof(buff));
-        printf(buff);
+        //Print from the server
+        std::cout << buff);
 }
 
 int main()
 {
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cli;
+    int sockfd
+    struct sockaddr_in servaddr;
 
     // socket create and varification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //Check that the socket is successfully created
+    //If not, error
     if (sockfd == -1) {
-        printf("socket creation failed...\n");
+        std::cout << "Socket creation failed...\n" << std::endl;;
         exit(0);
     }
-    else
-        printf("Socket successfully created..\n");
+
+    //If the socket is created, print. Used for debugging
+    /*else
+        std::cout << "Socket successfully created..\n");*/
+
+    //Zero out the server address
     bzero(&servaddr, sizeof(servaddr));
 
-    // assign IP, PORT
+    // assign IP and PORT information
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
 
     // connect the client socket to server socket
+    //Check that the connection is completed successfully
     if (connect(sockfd, (sockaddr*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("connection with the server failed...\n");
+        std::cout << "Connection with the server failed..."  << std::endl;
         exit(0);
     }
+    //If the connection is successful, print
     else
-        printf("connected to the server..\n");
+        std::cout << "Connected to the server" << std::endl;
 
-    //function for chat
-    func(sockfd);
+    //function to allow writing to server
+    foo(sockfd);
 
     // close the socket
     close(sockfd);
--- tcp_server.cpp
+++ tcp_server.cpp
@@ -10,185 +10,162 @@
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
 
 #define TRUE   1
-#define FALSE  0
-#define PORT 8888
+#define PORT 12345
+#define MAX_CLIENTS 2
+#define MAX 1024
 
-int main(int argc , char *argv[])
-{
+int main(int argc, char *argv[]){
     int opt = TRUE;
-    int master_socket , addrlen , new_socket , client_socket[2] , activity, i , valread , sd;
+    int sockfd, new_socket, client_socket[MAX_CLIENTS], i, sd;
     int max_sd;
     struct sockaddr_in address;
 
-    char buffer[1025];  //data buffer of 1K
+    char buffer[MAX];  //data buffer of 1K
 
     //set of socket descriptors
     fd_set readfds;
 
-    //a message
-    char *message = " received before ";
-
-    //initialise all client_socket[] to 0 so not checked
-    for (i = 0; i < 2; i++)
-    {
+    //initialize all client_socket[] to 0
+    for (i = 0; i < MAX_CLIENTS; i++){
         client_socket[i] = 0;
     }
 
     //create a master socket
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0));
 
-    //set master socket to allow multiple connections ,
-    //this is just a good habit, it will work without this
-    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
-          sizeof(opt)) < 0 )
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    //set master socket to allow multiple connections,
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
 
-    //type of socket created
+    //Assign IP and Port information
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
-    //bind the socket to localhost port 8888
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("bind failed");
+    //bind the socket to localhost port 12345
+    if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0){
+        std::cerr << "bind failed" << std::endl;
         exit(EXIT_FAILURE);
     }
-    printf("Listener on port %d \n", PORT);
 
-    //try to specify maximum of 3 pending connections for the master socket
-    if (listen(master_socket, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    //the server socket can only have two pending connections; one for each server
+    listen(sockfd, MAX_CLIENTS);
 
     //accept the incoming connection
     addrlen = sizeof(address);
     puts("Waiting for connections ...");
 
+    //Counter will be used to make sure that two messages have been received, on from each client
 	int counter = 0;
+    //Flag will indicate whether or not we are reading the first message
+    //If we are reading the first message, we copy it. If not, we concatenate it.
 	int flag = 0;
-	char temp[1025] = {'\0'};
+    //Temp used to create message to send to clients
+	char temp[MAX] = {'\0'};
 
-    while(counter < 2)
+    while(counter < MAX_CLIENTS)
     {
+        //Select setup from https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
         //clear the socket set
         FD_ZERO(&readfds);
 
-        //add master socket to set
-        FD_SET(master_socket, &readfds);
-        max_sd = master_socket;
+        //add server socket to set
+        FD_SET(sockfd, &readfds);
+        max_sd = sockfd;
 
         //add child sockets to set
-        for ( i = 0 ; i < 2 ; i++)
+        for ( i = 0 ; i < MAX_CLIENTS ; i++)
         {
             //socket descriptor
             sd = client_socket[i];
 
             //if valid socket descriptor then add to read list
             if(sd > 0)
-                FD_SET( sd , &readfds);
+                FD_SET( sd, &readfds);
 
             //highest file descriptor number, need it for the select function
             if(sd > max_sd)
                 max_sd = sd;
         }
 
-        //wait for an activity on one of the sockets , timeout is NULL ,
-        //so wait indefinitely
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+        //wait for an activity on one of the sockets
+        //Set the timeout to NULL so that we wait indefinitely for activity
+        activity = select( max_sd + 1, &readfds, NULL, NULL, NULL);
 
-        if ((activity < 0) && (errno!=EINTR))
-        {
-            printf("select error");
+        if ((activity < 0) && (errno != EINTR)){
+            std::cerr << "select error" << std::endl;
         }
 
-        //If something happened on the master socket ,
-        //then its an incoming connection
-        if (FD_ISSET(master_socket, &readfds))
-        {
-            if ((new_socket = accept(master_socket,
-                    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-            {
-                perror("accept");
+        //Something happening on the socket server indicates incoming connection
+        if (FD_ISSET(sockfd, &readfds)){
+            //Check that the incoming connection is accepted
+            //If not exit the program
+            if ((new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&sizeof(address))) < 0){
+                std::cerr << "accept" << std::endl;
                 exit(EXIT_FAILURE);
             }
 
-            //inform user of socket number - used in send and receive commands
-            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
-                  (address.sin_port));
-
-            //send new connection greeting message
-            /*if( send(new_socket, message, strlen(message), 0) != strlen(message) )
-            {
-                perror("send");
-            }
-
-            puts("Welcome message sent successfully");*/
-
             //add new socket to array of sockets
-            for (i = 0; i < 2; i++)
-            {
-                //if position is empty
-                if( client_socket[i] == 0 )
-                {
+            for (i = 0; i < MAX_CLIENTS; i++){
+                //Find empty index in array of sockets and add the new one
+                if( client_socket[i] == 0 ){
                     client_socket[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n" , i);
-
                     break;
                 }
             }
         }
 
         //else its some IO operation on some other socket
-        for (i = 0; i < 2; i++)
-        {
+        for (i = 0; i < MAX_CLIENTS; i++){
             sd = client_socket[i];
 
-            if (FD_ISSET( sd , &readfds))
-            {
-                //Check if it was for closing , and also read the
+            if (FD_ISSET( sd, &readfds)){
+                //Check if it was for closing, and also read the
                 //incoming message
-                if ((valread = read( sd , buffer, 1024)) == 0)
-                {
-                    //Somebody disconnected , get his details and print
-                    getpeername(sd , (struct sockaddr*)&address , \
-                        (socklen_t*)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n" ,
-                          inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+                if ((read( sd, buffer, MAX)) == 0){
+                    //Somebody disconnected, get his details and print
+                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&sizeof(address));
 
                     //Close the socket and mark as 0 in list for reuse
-                    close( sd );
+                    close(sd);
                     client_socket[i] = 0;
                 }
 
-                //Echo back the message that came in
-                else
-                {
+                else{
+                    //Reaching this loop implies a message was received
+                    //Counter indicates number of messages received
+                    //When counter reaches 2, the while loop will terminate
 					counter++;
+                    //Flag indicates first message. Used if we wanted to abstract this idea
+                    //Could have used the counter here and incremented after
+                    //First message is copied from buffer to temp
 					if(flag == 0) strcpy(temp, buffer);
                     else{
+                        //If we enter this loop we've
+                        //So temp is currently the first message received
+                        //Concatenate " received before " as requested
 						strcat(temp, " received before ");
+                        //The buffer now contains the second message
+                        //Concatenate the second message
 						strcat(temp, buffer);
 						strcat(temp, "\n");
 					}
+                    //Flag indicates whether or not a message received is the first
 					flag = 1;
                 }
             }
         }
-		if(counter == 2){
+		if(counter == MAX_CLIENTS){
+            //Write to the first client that joined the server
 			write(client_socket[0], temp, sizeof(temp));
+            //Write to the second client that joined the server
 			write(client_socket[1], temp, sizeof(temp));
+            //Indicate that both acknowledgement messages have been sent
+            std::cout << "Sent acknowledgement to both X and Y" << std::endl;
 		}
     }
+
+    //Close socket
+    close(sockfd);
 
     return 0;
 }
